f7611f8f40679a83de31281a726d39a7241a398b
Buglik <kubula243243@gmail.com>
2021-05-01 21:06:57 +0200

    added Create room request

---
+++ music_controller/frontend/src/components/JoinRoomPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class JoinRoomPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>Welcome to the join room page!</p>;
+  }
+}
